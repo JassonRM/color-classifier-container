519d958311f87628ffdcfc1c554eee9f4a0260c6
Jasson Rodriguez <jassonrm@icloud.com>
2020-09-11 01:34:19 -0600

    Add socket

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t color-classifier .
-docker run -it -v /home/jasson/Documents/color-classifier-container/volume:/myapp/output color-classifier
+docker run -it -p 8080:8080 -v /home/jasson/Documents/color-classifier-container/volume:/myapp/output color-classifier
--- src/main.c
+++ src/main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PORT 8080
 
 int main() {
     printf("Hello world!\n");
     char filename[] = "output/configuracion.config";
     printf("Path: %s\n", filename);
     FILE *input = fopen(filename, "r");
-    if(input == NULL){
+    if (input == NULL) {
         perror("Error while opening the file.\n");
         exit(EXIT_FAILURE);
-    }else{
+    } else {
         char ch;
-        while((ch = fgetc(input)) != EOF)
+        while ((ch = fgetc(input)) != EOF)
             printf("%c", ch);
         fclose(input);
     }
@@ -24,5 +30,50 @@ int main() {
     FILE *output = fopen(path, "w");
     fprintf(output, "Testing");
     fclose(output);
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                   &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *) &address,
+             sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *) &address,
+                             (socklen_t *) &addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024);
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    return 0;
+
     return 0;
 }

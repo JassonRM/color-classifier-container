f069942f8eed5b4e4a1a56c8305c099e14f69365
Jasson Rodriguez <jassonrm@icloud.com>
2020-09-19 19:35:32 -0600

    Read config file and assign permissions

--- src/main.c
+++ src/main.c
@@ -1,15 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
-#include <png.h>
 #include "image.h"
 #include <sys/stat.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
 
 #define PORT 8080
+
+void read_config_file(char trusted[][15], char accepted[][15]){
+    char filename[] = "output/configuracion.config";
+    printf("Path: %s\n", filename);
+    FILE *input = fopen(filename, "r");
+
+    if (input == NULL) {
+        perror("Error while opening the file.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        char *line;
+        size_t len = 0;
+
+        getline(&line, &len, input);
+        if (strcmp(line, "trusted=\n") == 0) {
+            int i = 0;
+            while (1) {
+                getline(&line, &len, input);
+                if (strcmp(line, "accepted=\n") == 0) {
+                    break;
+                } else {
+                    line[strlen(line) - 1] = '\0';
+                    strcpy(trusted[i], line);
+                    printf("Adding trusted: %s\n", trusted[i]);
+                    i++;
+                }
+            }
+            i = 0;
+            while (getline(&line, &len, input) != -1) {
+                line[strlen(line) - 1] = '\0';
+                printf("Adding accepted: %s\n", line);
+                strcpy(accepted[i], line);
+                i++;
+            }
+        } else {
+            printf("Wrong format\n");
+            printf("%s\n", line);
+            printf("trusted=");
+        }
+
+        fclose(input);
+    }
+}
+
+void read_socket_into_file(int socket, FILE *file){
+    // Read from socket
+    char socket_buffer[1024];
+    ssize_t bytes_read, totalBytes = 0;
+    while ((bytes_read = read(socket, socket_buffer, 1024)) > 0) {
+        totalBytes += bytes_read;
+        fwrite(socket_buffer, bytes_read, 1, file);
+    }
+    printf("Total bytes read: %ld bytes\n", totalBytes);
+}
 
 int main() {
     // Create required directories
@@ -26,19 +80,12 @@ int main() {
     snprintf(output_dir, sizeof output_dir, "%s%d%s", "output/", id, "/Not trusted");
     mkdir(output_dir, 0777);
 
-    char filename[] = "output/configuracion.config";
-    printf("Path: %s\n", filename);
-    FILE *input = fopen(filename, "r");
-    if (input == NULL) {
-        perror("Error while opening the file.\n");
-        exit(EXIT_FAILURE);
-    } else {
-        char ch;
-        while ((ch = fgetc(input)) != EOF)
-            printf("%c", ch);
-        fclose(input);
-    }
+    // Read config file
+    char trusted[20][15];
+    char accepted[20][15];
+    read_config_file(trusted, accepted);
 
+    // Create socket
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
@@ -76,45 +123,67 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Saved received file in memory
-    char *mem_buffer;
-    size_t file_size;
-    FILE *image = open_memstream(&mem_buffer, &file_size);
+    char *ip_address = inet_ntoa(address.sin_addr);
+    printf("IP address is: %s\n", ip_address);
+    bool ip_found = false;
 
-    // Read from socket
-    char socket_buffer[1024];
-    ssize_t bytes_read, totalBytes;
-    read(new_socket, filename, 30);
-    while((bytes_read = read(new_socket, socket_buffer, 1024)) > 0) {
-        totalBytes += bytes_read;
-        fwrite(socket_buffer, bytes_read, 1, image);
-    }
-    printf("Total bytes read: %ld bytes\n", totalBytes);
+    for(int i = 0; trusted[i][0] != '\0'; i++){
+        if(strcmp(trusted[i], ip_address) == 0){
+            // Saved received file in memory
+            char *mem_buffer;
+            size_t file_size;
+            FILE *image = open_memstream(&mem_buffer, &file_size);
+            char filename[30];
+            read(new_socket, filename, 30);
+            read_socket_into_file(new_socket, image);
+            printf("Filename: %s\n", filename);
 
-    // Classify file
-    read_png_file(image);
-    int result = process_png_file();
-    printf("Result: %d\n", result);
-    // Open file for writing
-    char path[100];
-    char *folder;
-    switch (result) {
-        case 0:
-            folder = "R";
+            // Classify file
+            read_png_file(image);
+            int result = process_png_file();
+            // Open file for writing
+            char path[100];
+            char *folder;
+            switch (result) {
+                case 0:
+                    folder = "R";
+                    break;
+                case 1:
+                    folder = "G";
+                    break;
+                case 2:
+                    folder = "B";
+                    break;
+                default:
+                    printf("Error");
+                    return -1;
+            }
+            printf("Result: %s\n", folder);
+            snprintf(path, sizeof path, "%s%d%s%s%s%s", "output/", id, "/", folder, "/", filename);
+            printf("Path: %s\n", path);
+            FILE *output = fopen(path, "wb");
+            fwrite(mem_buffer, file_size, 1, output);
+            fclose(output);
+            ip_found = true;
             break;
-        case 1:
-            folder = "G";
-            break;
-        case 2:
-            folder = "B";
-            break;
-        default: folder = "Not trusted";
+        }
     }
-    snprintf(path, sizeof path, "%s%d%s%s%s%s", "output/", id, "/", folder, "/", filename);
-    printf("Path: %s\n", path);
-    FILE *output = fopen(path, "wb");
-    fwrite(mem_buffer, file_size, 1, output);
-    fclose(output);
+    if(!ip_found) {
+        for (int i = 0; accepted[i][0] != '\0'; i++) {
+            if (strcmp(accepted[i], ip_address) == 0) {
+                char path[100];
+                char filename[30];
+                read(new_socket, filename, 30);
+                snprintf(path, sizeof path, "%s%d%s%s", "output/", id, "/Not trusted/", filename);
+                printf("Path: %s\n", path);
+                FILE *output = fopen(path, "wb");
+                read_socket_into_file(new_socket, output);
+                fclose(output);
+                break;
+            }
+        }
+    }
+    close(new_socket);
+    printf("Connection closed\n");
     return 0;
 }
-
